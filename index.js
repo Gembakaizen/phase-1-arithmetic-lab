10fa48f503565d62a3e78c7523af730a08152d86
Mohammed Yusuf <mohammedyusuf445@gmail.com>
2023-07-14 15:18:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 1000);
+
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+
+let max = Math.max(1, 5, 10, 20, 15);
 
 // Write your code here
